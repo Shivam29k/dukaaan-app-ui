dd1840f05c2ddeff17b7666646d86ab17a05ac8b
Shivam Kumar <skumarshivam50@gmail.com>
2024-02-15 22:54:00 +0530

    updated

--- src/components/Fotter.jsx
+++ src/components/Fotter.jsx
@@ -6,7 +6,7 @@ function Fotter() {
             <div>
                 ----- Made By<a className='pl-1 text-blue-700 font-medium' href="https://twitter.com/ShivamKrandom">Shivam Kumar</a>, inspired by<a className='px-1 text-blue-700 font-medium' href="https://twitter.com/subhashchy/status/1744308069751025894?t=MrLV-PSnfsgv0Hg2jTz5JA&s=08">Dukaan App UI</a>-----
             </div>
-            <div  className='text-blue-200' ><a href="">Get the source code</a></div>
+            <div  className='text-black-300 underline' ><a href="https://github.com/Shivam29k/dukaaan-app-ui">Get the source code</a></div>
         </div>
     )
 }
